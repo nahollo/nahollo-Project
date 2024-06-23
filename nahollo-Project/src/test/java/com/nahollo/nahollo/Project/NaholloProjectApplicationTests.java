997cc47b9e4a5b97971f890b030d01c23dfadc7e
NaHyeongJin <skgudwlsdlrj@gmail.com>
2024-06-24 01:34:47 +0900

    add Spring Boot
    add Spring Boot

---
+++ nahollo-Project/src/test/java/com/nahollo/nahollo/Project/NaholloProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nahollo.nahollo.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaholloProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
